8a3222d8dfe906067eadb01368a6969a53173f95
digimori <hanaorikuu@gmail.com>
2023-10-30 19:31:21 +0000

    Created ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -13,3 +14,22 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+class ProfileDetail(APIView):
+    """
+    Retrieve profile detail.
+    If it exists, fetch it, if not, run a 404
+    """
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+
