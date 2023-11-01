cc2ad9df50510681ad4c24eb252b498c1d0786bb
digimori <hanaorikuu@gmail.com>
2023-11-01 19:37:24 +0000

    Comments app created, Models and serializers created - Similar to Post model and serializer

---
+++ comments/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from comments.models import Comments
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.comments')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+    
+    class Meta:
+        model = Comments
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 
+            'created_at', 'content', 'updated_at', 'post'
+        ]
